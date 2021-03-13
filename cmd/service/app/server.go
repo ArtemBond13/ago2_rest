46042a6822560a17c196e8f22a865973be5f4008
Artem <garribond85@gmail.com>
2021-03-13 18:43:42 +0500

    changed handleGetOffer

--- cmd/service/app/server.go
+++ cmd/service/app/server.go
@@ -1,14 +1,17 @@
 package app
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/ArtemBond13/ago2_rest/pkg/offers"
 	"github.com/go-chi/chi"
+	"log"
 	"net/http"
 )
 
 type Server struct {
 	offersSvc *offers.Service
-	router chi.Router
+	router    chi.Router
 }
 
 func NewServer(offers *offers.Service, router chi.Router) {
@@ -19,29 +22,46 @@ func NewServer(offers *offers.Service, router chi.Router) {
 func (s *Server) Init() error {
 	s.router.Get("/offers", s.handleGetOffers)
 	s.router.Get("/offers/{id}", s.handleGetOffersByID)
-	s.router.Post("/offers", s.handleOffer)
+	s.router.Post("/offers", s.handleSaveOffer)
 	s.router.Delete("/offers/{id}", s.handleRemoveOfferByID)
 
 	return nil
 }
 
-func (s *Server) ServerHTTP(writer http.ResponseWriter, reader *http.Request)  {
+func (s *Server) ServerHTTP(writer http.ResponseWriter, reader *http.Request) {
 	s.router.ServeHTTP(writer, reader)
 }
 
-func (s *Server) handleGetOffers(writer http.ResponseWriter, reader *http.Request)  {
-	fmt.Print("not implemented")
+// у Get нет тела (только path, query и заголовки)
+func (s *Server) handleGetOffers(writer http.ResponseWriter, request *http.Request) {
+	items, err := s.offersSvc.All(request.Context())
+	if err != nil {
+		http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	// серилизуем
+	data, err := json.Marshal(items)
+	if err != nil {
+		http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	_, err = writer.Write(data)
+	if err != nil {
+		log.Println(err)
+	}
 }
 
-func (s *Server) handleGetOffersByID(writer http.ResponseWriter, reader *http.Request)  {
+func (s *Server) handleGetOffersByID(writer http.ResponseWriter, reader *http.Request) {
 	fmt.Println("not implemented")
 }
 
-func (s *Server) handleSaveOffer(writer http.ResponseWriter, reader *http.Request)  {
+func (s *Server) handleSaveOffer(writer http.ResponseWriter, reader *http.Request) {
 	fmt.Println("not implemented")
 }
 
-func (s *Server) handleRemoveOfferByID(writer http.ResponseWriter, reader *http.Request)  {
+func (s *Server) handleRemoveOfferByID(writer http.ResponseWriter, reader *http.Request) {
 	fmt.Println("not implemented")
 }
-
